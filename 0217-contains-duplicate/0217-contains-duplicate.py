4ec07a505cfef95649614948ea908ffd19a068de
Kanav <kanav.12@hotmail.com>
2023-06-08 18:17:36 -0400

    Time: 482 ms (74.17%), Space: 29.1 MB (27.07%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        numsSet = set(nums)
+        
+        if len(numsSet) != len(nums):
+            return True
+        
+        return False
