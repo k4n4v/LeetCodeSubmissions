5825ee6959e8df3ebb4606f61dfea6412e34715f
Kanav <kanav.12@hotmail.com>
2023-10-27 23:50:09 -0400

    Time: 3339 ms (14.41%), Space: 14 MB (90.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
         
-        prevMap = {}
-
-        for index, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], index]
-            prevMap[n] = index
-        return
+        for index1, num1 in enumerate(nums):
+            for index2, num2 in enumerate(nums[index1+1:]):
+                result = num1 + num2 
+                if result == target:
+                    return [index1, index1+index2+1]
