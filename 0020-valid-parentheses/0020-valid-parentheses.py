4302670c7ca7b84b38f22565f04b145ba2ffc066
Kanav <kanav.12@hotmail.com>
2024-05-02 23:44:17 -0400

    Time: 35 ms (57.39%), Space: 16.7 MB (22.80%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isValid(self, s: str) -> bool:
         
         stack = []
-        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
-        
+        closeToOpen = {"}" : "{", 
+                       ")" : "(", 
+                       "]" : "["}
+                
         for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+            if c in closeToOpen: # if this char is a closing bracket (every key of map is always a closing bracket)
+                if stack and stack[-1] == closeToOpen[c]: # make sure stack is not empty and value at the top is the matching opening bracket
                     stack.pop()
                 else:
                     return False
-            else:
+            else: # if c is an open bracket
                 stack.append(c)
-        
+                         
         return True if not stack else False
+            
