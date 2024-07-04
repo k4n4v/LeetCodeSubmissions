2de1de3fea28c5aa2d7048539b863f2d45344a8f
Kanav <kanav.12@hotmail.com>
2024-07-04 00:01:11 -0400

    Time: 55 ms (56.45%), Space: 16.6 MB (69.31%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,7 +10,7 @@ class Solution:
         curr = temp
         
         carry = 0
-        while l1 or l2 or carry :
+        while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
             
@@ -22,7 +22,6 @@ class Solution:
             curr.next = ListNode(val)
             
             # update pointers
-            
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
