77cf8deb2793ac6f1dd8567fd3a09ec5d9f25dee
Kanav <kanav.12@hotmail.com>
2024-10-02 15:36:05 -0400

    Time: 648 ms (77.48%), Space: 20.7 MB (74.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,29 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
         res = []
-        nums.sort()
-                
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
+        nums.sort() # takes O(nlogn) time
+        
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i - 1]:
                 continue
-                
+            
             l, r = i + 1, len(nums) - 1
+            
             while l < r:
-                threeSum = a + nums[l] + nums[r]
+                threeSum = num + nums[l] + nums[r]
+                
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
                     l += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    res.append([num, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l -1] and l < r:
+                    while nums[l] == nums[l-1] and l < r:
                         l += 1
         return res
-                    
+    
+    # total time complexity O(nlogn) + n^2 = O(n^2)
+    # space could be O(n) or O(1) based on sorting in library
+    
