a40b4716fb4b2f0f7d9c87ce684001a4ca00e94e
Kanav <kanav.12@hotmail.com>
2024-10-02 13:16:41 -0400

    Time: 40 ms (81.78%), Space: 17 MB (84.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         
-        s_converted = "".join(c for c in s if c.isalnum()).lower()
+        s_converted = ""
+        
+        for c in s:
+            if c.isalnum():
+                s_converted += c
+            
+        s_converted = s_converted.lower()
         
         for i in range(len(s_converted)//2):
-            if s_converted[i] == s_converted[len(s_converted)-i-1]:
-                continue
-            else:
+            if s_converted[i] != s_converted[len(s_converted)-i-1]:
                 return False
             
         return True
