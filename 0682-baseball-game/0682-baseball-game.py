8cf867140e043f6192cc3be4369a8b4e86572cac
Kanav <kanav.12@hotmail.com>
2024-10-03 17:04:23 -0400

    Time: 38 ms (87.42%), Space: 16.9 MB (11.10%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        stack = []
+        
+        for i, op in enumerate(operations):
+            
+            if op == "C":
+                stack.pop()
+                
+            elif op == "D":
+                stack.append(stack[-1]*2)
+                
+            elif op == "+":
+                stack.append(stack[-1]+stack[-2])
+                
+            else:
+                stack.append(int(op))
+        
+        return sum(stack)
