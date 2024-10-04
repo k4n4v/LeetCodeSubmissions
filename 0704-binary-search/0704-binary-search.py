32663f36b2f50e31d26cee6ca87f7dfd411ebc5e
Kanav <kanav.12@hotmail.com>
2024-10-04 17:10:33 -0400

    Time: 190 ms (62.84%), Space: 18.3 MB (6.27%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -2,15 +2,15 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         l, r = 0, len(nums) - 1
-                
-        while l <= r: # end of loop indicates l pointer is greater than right meaning there is no more integers in array
+        
+        while l <= r:
             m = (l + r) // 2
-            if nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
+            
+            if nums[m] < target:
                 l = m + 1
+            elif nums[m] > target:
+                r = m - 1
             else:
                 return m
-            
         return -1
                 
