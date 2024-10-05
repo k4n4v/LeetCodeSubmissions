9e0bc545e6e567d34be6a2527b258e4452f451f9
Kanav <kanav.12@hotmail.com>
2024-10-05 16:23:19 -0400

    Time: 2956 ms (34.47%), Space: 140.2 MB (17.62%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -4,44 +4,27 @@ class Solution:
         # 1. DFS with recursion
         # 2. DFS with stack (iterative)
         # 3. BFS with deque
-        
+
         if source == destination: # base case
             return True
         
-        # Adjacency list using dict
-        graph = defaultdict(list)
+        seen = set() # need to know what nodes we have visted to avoid infiate loop
+        graph = defaultdict(list) # adjaceny list to keep track of node to node
+        seen.add(source) # starting at source so add to seen
         
-        for u, v in edges: # u and v are the two nodes (or vertices) connected by an edge.
+        for u, v in edges: # create a graph based on the edge connections
             graph[u].append(v)
-            graph[v].append(u) # because bi-dircetional
-
-        seen = set()
-        seen.add(source)
-        stack = [source]
-        
-        while stack:
-            node = stack.pop()
-            if node == destination:
-                return True
-            for nei_node in graph[node]:
-                if nei_node not in seen:
-                    seen.add(nei_node)
-                    stack.append(nei_node)
-                    
-        return False
-        
-        # Time: O(N + E)
-        # Space: O(N + E)
-        
-#         def dfs(i):
-#             if i == destination:
-#                 return True
+            graph[v].append(u) # bi - directional so we need to add v for u
             
-#             for nei_node in graph[i]:
-#                 if nei_node not in seen:
-#                     seen.add(nei_node)
-#                     if dfs(nei_node):
-#                         return True
-#             return False
-        
-        
+        def dfs(node): 
+            if node == destination: # we found the destination
+                return True
+            
+            for nei_node in graph[node]: # check the neighbour nodes of the node
+                if nei_node not in seen:
+                    seen.add(nei_node) # mark that we have seen the node
+                    if dfs(nei_node): # search the neighbnour nodes of that node
+                        return True # return true if we found it in the neighbours nodes
+            return False # did not find
+            
+        return dfs(source)
