5c15173591bdf4b8fde1e846b7df0686c860d715
Kanav <kanav.12@hotmail.com>
2024-10-06 19:20:07 -0400

    Time: 44 ms (7.78%), Space: 16.5 MB (20.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {0:1,1:2}
+        memo = {1:1,2:2}
         
         def f(x):
             if x in memo:
@@ -9,4 +9,4 @@ class Solution:
                 memo[x] = f(x-1) + f(x-2)
                 return memo[x]
             
-        return f(n-1)
+        return f(n)
