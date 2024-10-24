270f3c9778b802e20192be40ba6b858fc210d940
Kanav <kanav.12@hotmail.com>
2024-10-24 16:37:30 -0400

    Time: 0 ms (100.00%), Space: 16.6 MB (40.07%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,13 +3,16 @@ class Solution:
         
         if needle == "":
             return 0
-        
-        l = 0
-        
-        for i in range(len(haystack) + 1 - len(needle)):
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-                if j == len(needle) - 1:
-                    return i
+
+        # We only need to loop till len_h - len_n + 1 because there's no point in checking the last few characters where the needle can't fit
+        for i in range(len(haystack) - len(needle) + 1):
+            # Two pointers to compare the haystack and the needle
+            j = 0
+            while j < len(needle) and haystack[i + j] == needle[j]:
+                j += 1
+            # If we matched the whole needle, return the starting index
+            if j == len(needle):
+                return i
+
+        # If no match is found, return -1
         return -1
